d65b583c4aecf0358c67f9b9169652b10e8140be
Rohit Verma <rohit.verma272727@gmail.com>
2025-02-12 17:17:58 +0530

    feat: Added routing using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Auth from "./pages/auth";
+import Chats from "./pages/chat";
+import Profile from "./pages/profile";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="text-blue-400 text-3xl">Hello World</div>
-      <Button>Click Me</Button>
-    </>
+    <Routes>
+      <Route path="/auth" element={<Auth />}></Route>
+      <Route path="/chats" element={<Chats />}></Route>
+      <Route path="/profile" element={<Profile />}></Route>
+
+      <Route path="*" element={<Navigate to="/auth" />}></Route>
+    </Routes>
   );
 }
 
---
+++ frontend/src/pages/auth/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Auth = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className="h-[80vh] w-[60vw] bg-white border-white rounded-2xl shadow-2xl">
+        <div className="flex items-center justify-center p-4 gap-4">
+          <div className="text-4xl">Welcome ✌️</div>
+          <div>Hii</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
