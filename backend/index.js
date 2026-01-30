6bba8257a13d80ebf104a49b2facf8642c602e8a
Rohit Verma <rohit.verma272727@gmail.com>
2026-01-30 23:07:35 +0530

    fix: allow android cors origins

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,9 @@ dotenv.config();
 //middlewares
 const allowedOrigins = [
   process.env.FRONTEND_URL,
-  "http://localhost:5173",
+  "http://localhost:5173",     
+  "http://localhost",         
+  "capacitor://localhost",
 ];
 
 const corsOptions = {
