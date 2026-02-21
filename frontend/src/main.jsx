6862a1fd7c1b1e99a090fa5641d0c783f6061f03
Rohit Verma <rohit.verma272727@gmail.com>
2026-02-21 21:31:20 +0530

    refactor: Remove import of Audio and Video call screen compoment in main.jsx to lazy load these components

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
 import { SocketProvider } from "@/context/SocketContext";
 import IncomingCallOverlay from "./components/IncomingCallOverlay";
-import AudioCallScreen from "./components/AudioCallScreen";
-import VideoCallScreen from "./components/VideoCallScreen";
 import CallContainer from "./pages/call";
 
 createRoot(document.getElementById("root")).render(
